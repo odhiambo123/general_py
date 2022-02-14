e6a80d5d728241c74425f622834e1bd64f4ae30f
David Odhiambo <odhiambodave@live.com>
2022-02-14 10:00:44 -0800

    update

--- src/general_py/machine_deep.py
+++ src/general_py/machine_deep.py
@@ -1,12 +1,14 @@
 from sklearn.datasets import fetch_olivetti_faces
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 faces = fetch_olivetti_faces()
 
 # setup the figure
 
 fig = plt.figure(figsize=(6,6))#figure size in inches
-fig = subplots_adjust(left=0, right=1, bottom=0, top=1,hspace=0.05,wspace=0.05)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
 # plot the digits: each image is 8x8 pixels
 for i in range(64):
@@ -14,7 +16,7 @@ for i in range(64):
     ax.imshow(faces.images[i], cmap=plt.cm.bone, interpolation='nearest')
 
     # label the image with the target value
-    ax.text(0, 7, str(faces.target[i]))
+    print(ax.text(0, 7, str(faces.target[i])))
 
 
 if __name__=='__main__':
